171e4834c7bb3a0ecbd5cce486a32a7bb664b6c7
Arbaz6400 <100441449+Arbaz6400@users.noreply.github.com>
2025-11-07 18:52:12 +0530

    Update ProducerApp.java

--- kafka-demo/target/classes/com/example/kafka/ProducerApp.java
+++ kafka-demo/target/classes/com/example/kafka/ProducerApp.java
@@ -13,6 +13,8 @@ public class ProducerApp {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
